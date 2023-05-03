6e8da6e4278fbb8f107c00f4df075b46782938c7
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2023-05-03 16:18:20 +0200

    MidiQol Compat

--- src/handlers/rollHandler.js
+++ src/handlers/rollHandler.js
@@ -2,16 +2,37 @@ import {getSetting} from "../utils/settings.js";
 import {Server} from "../utils/server.js";
 
 export function registerHanlder(){
-    Hooks.on("createChatMessage",(event)=>{
+    Hooks.on("createChatMessage", (event) => {
         const msg = event
-        if(msg.isRoll&&(msg.whisper.length === 0))
-        {
+        if (msg.isRoll && (msg.whisper.length === 0)) {
             const formula = msg.roll?.formula;
-            const result = msg.roll?.result;
+            const result = msg.roll?.total;
 
-            if(getSetting("sendRollsToChat")){
-                Server.getServer().sendMessage("Rolled "+formula+" and got a "+result+"!");
+            if (getSetting("sendRollsToChat")) {
+                Server.getServer().sendMessage("Rolled " + formula + " and got a " + result + "!");
             }
+        }
+    })
+
+    Hooks.on('midi-qol.AttackRollComplete', (workflow) => {
+        const attackRoll = workflow.attackRoll;
+        const message = game.messages.get(workflow.itemCardId)
+        const formula = attackRoll.formula;
+        const result = attackRoll.total;
+
+        if (message.whisper.length === 0 && getSetting("sendRollsToChat")) {
+            Server.getServer().sendMessage("Rolled " + formula + " and got a " + result + "!");
+        }
+    })
+
+    Hooks.on('midi-qol.DamageRollComplete', (workflow) => {
+        const attackRoll = workflow.damageRoll;
+        const message = game.messages.get(workflow.itemCardId)
+        const formula = attackRoll.formula;
+        const result = attackRoll.total;
+
+        if (message.whisper.length === 0 && getSetting("sendRollsToChat")) {
+            Server.getServer().sendMessage("Rolled " + formula + " and got a " + result + "!");
         }
     })
 }
