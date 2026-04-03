1d4d60df97c1fc21ce8c9e3e9353d8854bae0828
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2026-04-03 16:58:28 +0200

    lint

--- src/svelte/components/TriggerConfig.svelte
+++ src/svelte/components/TriggerConfig.svelte
@@ -1,23 +1,23 @@
 <svelte:options runes={true} />
 
-<script lang="ts">
-	import type { TriggerConfig, TriggerMacro } from "../../utils/types.ts";
-	import { onMount } from "svelte";
+<script lang='ts'>
+	import type { TriggerConfig, TriggerMacro } from '../../utils/types.ts';
+	import { onMount } from 'svelte';
 	import {
 		downloadTriggerMacro,
 		uploadTriggerMacro,
-	} from "../../server/trigger_api.ts";
-	import { createMacro, getGame } from "../../utils/helpers.ts";
-	import { getSetting, setSetting } from "../../utils/settings.ts";
+	} from '../../server/trigger_api.ts';
+	import { createMacro, getGame } from '../../utils/helpers.ts';
+	import { getSetting, setSetting } from '../../utils/settings.ts';
 
 	let { trigger = $bindable() }: { trigger: TriggerConfig } = $props();
 
 	let macro: Macro | undefined = $state();
 
 	function getMacro() {
-		const macro_map = getSetting("chat-trigger-macros") as TriggerMacro[];
+		const macro_map = getSetting('chat-trigger-macros') as TriggerMacro[];
 		const macro_name = macro_map?.find(
-			(data) => data.id === trigger.id,
+			data => data.id === trigger.id,
 		)?.macro;
 
 		if (!macro_name) {
@@ -32,11 +32,11 @@
 	}
 
 	async function deleteMacro() {
-		const macros = getSetting("chat-trigger-macros") as TriggerMacro[];
-		const index = macros.findIndex((entry) => entry.id === trigger.id);
+		const macros = getSetting('chat-trigger-macros') as TriggerMacro[];
+		const index = macros.findIndex(entry => entry.id === trigger.id);
 		macros.splice(index, 1);
 		macro = undefined;
-		await setSetting("chat-trigger-macros", macros);
+		await setSetting('chat-trigger-macros', macros);
 	}
 
 	async function dropMacro(event: DragEvent) {
@@ -44,21 +44,21 @@
 			return;
 		}
 		try {
-			const data = JSON.parse(event.dataTransfer.getData("text/plain"));
+			const data = JSON.parse(event.dataTransfer.getData('text/plain'));
 			if (
-				Object.keys(data).includes("type") &&
-				Object.keys(data).includes("uuid")
+				Object.keys(data).includes('type')
+				&& Object.keys(data).includes('uuid')
 			) {
-				if (data.type === "Macro") {
+				if (data.type === 'Macro') {
 					const macro_data = (game as ReadyGame).macros.get(
-						data.uuid.split(".")[1],
+						data.uuid.split('.')[1],
 					);
 					if (macro_data && macro_data.id) {
 						const macro_map = getSetting(
-							"chat-trigger-macros",
+							'chat-trigger-macros',
 						) as TriggerMacro[];
 						const macro_entry = macro_map?.find(
-							(data) => data.id === trigger.id,
+							data => data.id === trigger.id,
 						);
 						if (macro_entry) {
 							macro_entry.macro = macro_data.id;
@@ -68,7 +68,7 @@
 								macro: macro_data.id,
 							});
 						}
-						await setSetting("chat-trigger-macros", macro_map);
+						await setSetting('chat-trigger-macros', macro_map);
 					}
 					macro = macro_data;
 				}
@@ -83,11 +83,11 @@
 			const res = await uploadTriggerMacro(trigger.id, macro.command);
 			if (res) {
 				ui.notifications?.info(
-					`${(game as ReadyGame).i18n.localize("ethereal-plane.macro.upload.success")}`,
+					`${(game as ReadyGame).i18n.localize('ethereal-plane.macro.upload.success')}`,
 				);
 			} else {
 				ui.notifications?.warn(
-					`${(game as ReadyGame).i18n.localize("ethereal-plane.macro.upload.failure")}`,
+					`${(game as ReadyGame).i18n.localize('ethereal-plane.macro.upload.failure')}`,
 				);
 			}
 		}
@@ -102,10 +102,10 @@
 				macro = await createMacro(trigger.title, res);
 				if (macro && macro.id) {
 					const macro_map = getSetting(
-						"chat-trigger-macros",
+						'chat-trigger-macros',
 					) as TriggerMacro[];
 					const macro_entry = macro_map?.find(
-						(data) => data.id === trigger.id,
+						data => data.id === trigger.id,
 					);
 					if (macro_entry) {
 						macro_entry.macro = macro.id;
@@ -115,15 +115,15 @@
 							macro: macro.id,
 						});
 					}
-					await setSetting("chat-trigger-macros", macro_map);
+					await setSetting('chat-trigger-macros', macro_map);
 				}
 			}
 			ui.notifications?.info(
-				`${(game as ReadyGame).i18n.localize("ethereal-plane.macro.download.success")}`,
+				`${(game as ReadyGame).i18n.localize('ethereal-plane.macro.download.success')}`,
 			);
 		} else {
 			ui.notifications?.warn(
-				`${(game as ReadyGame).i18n.localize("ethereal-plane.macro.download.failure")}`,
+				`${(game as ReadyGame).i18n.localize('ethereal-plane.macro.download.failure')}`,
 			);
 		}
 	}
@@ -133,51 +133,51 @@
 	});
 </script>
 
-<div class="label">
+<div class='label'>
 	{#if trigger.active}
-		<i class="fas fa-check"></i>
+		<i class='fas fa-check'></i>
 	{:else}
-		<i class="fas fa-x"></i>
+		<i class='fas fa-x'></i>
 	{/if}
 </div>
-<div class="name">
+<div class='name'>
 	<span>
 		{trigger.title}
 	</span>
 </div>
-<div class="created date">
+<div class='created date'>
 	<span>
 		{new Date(trigger.created_at).toLocaleString()}
 	</span>
 </div>
-<div class="updated date">
+<div class='updated date'>
 	<span>
 		{new Date(trigger.updated_at).toLocaleString()}
 	</span>
 </div>
-<div class="macro-area">
-	<button aria-label="upload macro" onclick={uploadMacro} disabled={!macro}>
-		<i class="fas fa-upload"></i>
+<div class='macro-area'>
+	<button aria-label='upload macro' onclick={uploadMacro} disabled={!macro}>
+		<i class='fas fa-upload'></i>
 	</button>
-	<button aria-label="download macro" onclick={downloadMacro}>
-		<i class="fas fa-download"></i>
+	<button aria-label='download macro' onclick={downloadMacro}>
+		<i class='fas fa-download'></i>
 	</button>
 	<section
-		role="none"
-		class="macro"
+		role='none'
+		class='macro'
 		onauxclick={deleteMacro}
 		onclick={openMacro}
 		ondrop={dropMacro}
 	>
 		{#if macro}
-			<section class="macro">
-				<img alt="Macro Icon" src={macro.img ?? ""} />
+			<section class='macro'>
+				<img alt='Macro Icon' src={macro.img ?? ''} />
 			</section>
 		{/if}
 	</section>
 </div>
 
-<style lang="scss">
+<style lang='scss'>
 	.name {
 		font-size: 18px;
 		font-weight: bold;
